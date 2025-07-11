de0d3a3f4c5443f3366c382c92981989e560d8c4
sharad <sh244392.dal.ca>
2025-07-11 16:51:59 -0400

    Implemented CategoryRepository class and its interface

---
+++ Bulky.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,37 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+	internal class CategoryRepository : Repository<Category>, ICategoryRepository
+	{
+		//CategoryRepository Inherits shared data access logic methods from a generic base class (Repository<Category>) and
+		//Implements custom logic for methods defined in the interface ICategoryRepository
+
+		private ApplicationDbContext _db;
+		public CategoryRepository(ApplicationDbContext db) : base(db) {
+			//process flow
+			//step 1: We create an instance of CategoryRepository, probably via Dependency Injection:
+			//var categoryRepo = new CategoryRepository(applicationDbContext);
+			//step 2: Constructor of CategoryRepository is called with applicationDbContext as the db parameter
+			//step 3: : base(db) passes this db up to the constructor of the base class Repository<T>
+			//step 4: db is stored in base class and in CategoryRepository
+			//Step 5: This db reference will be used to perform crud operation in both the classes
+			_db = db;
+		}
+		public void Save() {
+			_db.SaveChanges();
+		}
+
+		public void Update(Category category) {
+			_db.Categories.Update(category);
+		}
+	}
+}
---
+++ Bulky.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+	internal interface ICategoryRepository : IRepository<Category>
+	{
+		//This interface will get all the methods defined in IRepository
+		//and in addition, implement 2 new methods i.e. Update and Save
+		void Update(Category category);
+		void Save();
+	}
+}
