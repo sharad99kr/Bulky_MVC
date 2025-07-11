c40b30561143c9fd1d0ec608d93580c03d1f28f7
sharad <sh244392.dal.ca>
2025-07-11 16:14:30 -0400

    Implemented Repository class and its Interface

---
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+	public interface IRepository<T> where T : class
+	{
+
+		//T - Category or any generic model on which we perform crud operation
+		// Retriieve categories
+		IEnumerable<T> GetAll();
+		T Get(Expression<Func<T,bool>> filter); //this supports link operation to apply condition if needed
+		void Add(T entity);
+		void Remove(T entity);
+		void RemoveRange(IEnumerable<T> entity);
+
+		//void Update(T entity); //usually update has unique operation, like update few selected properties
+		//and it varies from model to model, so we will keep update outside the repository 
+
+	}
+}
