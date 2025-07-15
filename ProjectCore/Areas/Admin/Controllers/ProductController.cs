3e6081bdc5201f138004a64c990fc028c1490068
sharad <sh244392.dal.ca>
2025-07-15 13:53:48 -0400

    deleted edit view, modified create view to include edit and renamed it to upsert. Made related action method changes in ProductController class

--- ProjectCore/Areas/Admin/Controllers/ProductController.cs
+++ ProjectCore/Areas/Admin/Controllers/ProductController.cs
@@ -23,7 +23,7 @@ namespace ProjectCore.Areas.Admin.Controllers
 			return View(objProductList);
         }
 
-        public IActionResult Create()
+        public IActionResult Upsert(int? id) //update+insert
         {
 			//we need to pass category list as well at this location.
 			//Step 1 would be to fetch all available categories with name and id
@@ -48,12 +48,20 @@ namespace ProjectCore.Areas.Admin.Controllers
                 CategoryList = CategoryList,
                 Product = new Product()
             };
-			return View(productVM); 
+            if(id == null || id == 0) {
+                //create
+                return View(productVM);
+            } else {
+                //update
+                productVM.Product = unitOfWork.Product.Get(u=>u.Id==id);
+				return View(productVM);
+			}
+			
         }
 
 
         [HttpPost]
-        public IActionResult Create(ProductVM productVM)
+        public IActionResult Upsert(ProductVM productVM, IFormFile? file)
         {
             if (ModelState.IsValid)
             {
@@ -71,42 +79,6 @@ namespace ProjectCore.Areas.Admin.Controllers
                                         });
 				return View(productVM);
 			}
-        }
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            //Product categoryFromDb = _db.Categories.Find(id);
-            //alternate ways to do a query on id. Using Linq
-            //Product categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
-            //Product categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
-
-            Product productFromDb = unitOfWork.Product.Get(u => u.Id == id);
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(productFromDb);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-            //we have to make sure obj has id which is set to 1(because we sent data with name id, if it was say categoryId, value will be 0), 
-            //if it is set to 0, the db will create new field instead
-            //of updating the current row. To solve this we create hidden id in Edit html file
-
-            if (ModelState.IsValid)
-            {
-                unitOfWork.Product.Update(obj);
-                unitOfWork.Save();
-                return RedirectToAction("Index"); //this can be used to redirect pages in same controller. If we want to go to another
-                //controller,we can pass the name of controller as second parameter along with action name
-            }
-
-            return View();
         }
 
         public IActionResult Delete(int? id)
