12692ae30447702249f1db49805c618dd0d974b8
sharad <sh244392.dal.ca>
2025-07-17 00:17:09 -0400

    used sweetalert2 to have a confirmation popup before delte. Also converted delete operation into HttpDelete call

--- ProjectCore/Areas/Admin/Controllers/ProductController.cs
+++ ProjectCore/Areas/Admin/Controllers/ProductController.cs
@@ -132,7 +132,7 @@ namespace ProjectCore.Areas.Admin.Controllers
             return Json(new { data = objProductList });
         }
 
-		
+        [HttpDelete]
 		public IActionResult Delete(int? id) {
 
             var productToBeDeleted = unitOfWork.Product.Get(u=>u.Id == id);
--- ProjectCore/wwwroot/js/product.js
+++ ProjectCore/wwwroot/js/product.js
@@ -1,4 +1,5 @@
-﻿
+﻿var dataTable;
+
 $(document).ready(function () {
     loadDataTable();
 });
@@ -17,7 +18,7 @@ function loadDataTable() {
                         "render": function (data) {
                             return `<div class="w-75 btn-group" role = "group">
                                 <a href = "/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i> EDIT</a >
-                                <a href = "/admin/product/delete/${data}" class="btn btn-danger mx-2"><i class="bi bi-trash-fill"></i> DELETE</a >
+                                <a onClick = "Delete( '/admin/product/delete/${data}')" class="btn btn-danger mx-2"><i class="bi bi-trash-fill"></i> DELETE</a >
                                 </div >`
                         },"width": "25%"
             }]
@@ -25,3 +26,26 @@ function loadDataTable() {
 
 }
 //coulmns count and value must match with view or else datatables will not work
+
+function Delete(url) {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    dataTable.ajax.reload();
+                    toastr.success(data.message);
+                }
+            })
+        }
+    });
+}
