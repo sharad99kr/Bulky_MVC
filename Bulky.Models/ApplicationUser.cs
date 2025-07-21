70559e8c220013ee6df0370e9a215b6ab6d1ea15
sharad <sh244392.dal.ca>
2025-07-21 15:25:22 -0400

    added application user model to extend Identity User, ran migration to update the db

---
+++ Bulky.Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Models
+{
+    public class ApplicationUser: IdentityUser //we see [Discriminator] column added by default. Its value help to identify if user is Application user or Identity User
+    {
+        [Required]
+        public int Name { get; set; }
+        public string? StreetAddress { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? PostalCode { get; set; }
+
+    }
+}
