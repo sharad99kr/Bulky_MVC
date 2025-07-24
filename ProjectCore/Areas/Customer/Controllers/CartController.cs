0e1c65eae76d0aedf4b5f255d8ea0351cab1b7b2
sharad <sh244392.dal.ca>
2025-07-24 00:55:19 -0400

    added shopping cart view, controller and shopping icon on main page layout

---
+++ ProjectCore/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectCore.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
