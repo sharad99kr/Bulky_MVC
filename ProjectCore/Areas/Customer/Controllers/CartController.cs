5e34afd30edc57050208f377d90e2157c1c530f0
sharad <sh244392.dal.ca>
2025-07-24 02:51:29 -0400

    added plus, minus and remove action methods for shopping cart

--- ProjectCore/Areas/Customer/Controllers/CartController.cs
+++ ProjectCore/Areas/Customer/Controllers/CartController.cs
@@ -32,6 +32,35 @@ namespace ProjectCore.Areas.Customer.Controllers
             return View(ShoppingCartVM);
         }
 
+        public IActionResult Plus(int cartId) {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            cartFromDb.Count++;
+            _unitOfWork.ShoppingCart.Update(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Minus(int cartId) {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            if(cartFromDb.Count <= 1) {
+                _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            } else {
+                cartFromDb.Count--;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+            
+            
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int cartId) {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
         private double GetPriceBasedOnQuantity(ShoppingCart cart) { 
             if(cart.Count <= 50) {
                 return cart.Product.Price;
