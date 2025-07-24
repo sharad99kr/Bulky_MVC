e59afe0f32d1ee439769a809a0347ab871becfb7
sharad <sh244392.dal.ca>
2025-07-24 03:04:30 -0400

    added summary page view

--- ProjectCore/Areas/Customer/Controllers/CartController.cs
+++ ProjectCore/Areas/Customer/Controllers/CartController.cs
@@ -32,6 +32,10 @@ namespace ProjectCore.Areas.Customer.Controllers
             return View(ShoppingCartVM);
         }
 
+        public IActionResult Summary() {
+            return View();
+        }
+
         public IActionResult Plus(int cartId) {
             var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
             cartFromDb.Count++;
