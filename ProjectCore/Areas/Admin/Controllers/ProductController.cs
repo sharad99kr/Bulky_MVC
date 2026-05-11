bd6246ee708ae85ca696ef6d54c027028d1f0808
sharad <sharad99kr@gmail.com>
2026-05-11 14:18:18 -0400

    Updated upload and delete methods for product images to store them directly on azure blob storage instead of local streaming folder

--- ProjectCore/Areas/Admin/Controllers/ProductController.cs
+++ ProjectCore/Areas/Admin/Controllers/ProductController.cs
@@ -8,6 +8,8 @@ using Microsoft.IdentityModel.Tokens;
 using Bulky.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Humanizer;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 
 namespace ProjectCore.Areas.Admin.Controllers
 {
@@ -16,121 +18,106 @@ namespace ProjectCore.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork unitOfWork;
-        private readonly IWebHostEnvironment webHostEnvironment; //this is to access www root folder for images
-        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
-        {
-            //In constructor we are requesting for the implementation of ApplicationDbContext
+        private readonly IWebHostEnvironment webHostEnvironment;
+        private readonly IConfiguration _configuration;
+
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment, IConfiguration configuration) {
             this.unitOfWork = unitOfWork;
             this.webHostEnvironment = webHostEnvironment;
-        }
-        public IActionResult Index()
-        {
-            List<Product> objProductList = unitOfWork.Product.GetAll(includeProperties:"Category").ToList();
-            
-			return View(objProductList);
+            _configuration = configuration;
         }
 
-        public IActionResult Upsert(int? id) //update+insert
-        {
-			//we need to pass category list as well at this location.
-			//Step 1 would be to fetch all available categories with name and id
-			//We will use projections in EF core to achieve it
-			IEnumerable<SelectListItem> CategoryList = unitOfWork
-														.Category
-														.GetAll()
-														.Select(u => new SelectListItem {
-															Text = u.Name,
-															Value = u.Id.ToString()
-														});
-			//step2 will be to pass this object(CategoryList) to the view
-			//we can pass data that are not in model using 3 ways : 1.ViewBag 2.ViewData 3.TempData
-			//1. Data passed as ViewBag persists only for current http request and data is lost if there is redirection
-			//2. ViewData is similar to ViewBag but it's value must be type cast before use
-            //3. TempData uses session to store data. It's value also needs to be type cast before use. It can be used for error/validations messages as well
-			//Usually we avoid using them and use ViewModels instead. This is because views are tightly bound wwith models and it is hard to identify where the extra data is coming from.
-            //ViewBag.CategoryList = CategoryList;
-			//ViewData["CategoryList"] = CategoryList;
+        private BlobContainerClient GetBlobContainerClient() {
+            string connectionString = _configuration["AzureStorage:ConnectionString"];
+            string containerName = _configuration["AzureStorage:ContainerName"];
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+            containerClient.CreateIfNotExists(PublicAccessType.Blob);
+            return containerClient;
+        }
+
+        public IActionResult Index() {
+            List<Product> objProductList = unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+            return View(objProductList);
+        }
+
+        public IActionResult Upsert(int? id) {
+            IEnumerable<SelectListItem> CategoryList = unitOfWork
+                .Category
+                .GetAll()
+                .Select(u => new SelectListItem {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                });
 
             ProductVM productVM = new() {
                 CategoryList = CategoryList,
                 Product = new Product()
             };
+
             if(id == null || id == 0) {
-                //create
                 return View(productVM);
             } else {
-                //update
-                productVM.Product = unitOfWork.Product.Get(u=>u.Id==id, includeProperties:"ProductImages");
-				return View(productVM);
-			}
-			
+                productVM.Product = unitOfWork.Product.Get(u => u.Id == id, includeProperties: "ProductImages");
+                return View(productVM);
+            }
         }
 
-
         [HttpPost]
-        public IActionResult Upsert(ProductVM productVM, List<IFormFile> files)
-        {
-            if (ModelState.IsValid)
-            {
-				if(productVM.Product.Id == 0) {
-					//No product Id(primary key) means it is new product
-					unitOfWork.Product.Add(productVM.Product);
-				} else {
-					unitOfWork.Product.Update(productVM.Product);
-				}
-				unitOfWork.Save();
+        public IActionResult Upsert(ProductVM productVM, List<IFormFile> files) {
+            if(ModelState.IsValid) {
+                if(productVM.Product.Id == 0) {
+                    unitOfWork.Product.Add(productVM.Product);
+                } else {
+                    unitOfWork.Product.Update(productVM.Product);
+                }
+                unitOfWork.Save();
 
-				string wwwRootPath = webHostEnvironment.WebRootPath;
                 if(files != null) {
-                    foreach(IFormFile file in files) { 
-                        string fileName=Guid.NewGuid().ToString()+Path.GetExtension(file.FileName); 
-                        string productPath=@"images\product\product-"+productVM.Product.Id;
-                        string filePath=Path.Combine(wwwRootPath, productPath);
+                    var containerClient = GetBlobContainerClient();
 
-                        if(!Directory.Exists(filePath)) { 
-                            Directory.CreateDirectory(filePath);
-                        }
+                    foreach(IFormFile file in files) {
+                        string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                        string blobPath = $"product/product-{productVM.Product.Id}/{fileName}";
+                        BlobClient blobClient = containerClient.GetBlobClient(blobPath);
 
-                        using(var filestream = new FileStream(Path.Combine(filePath, fileName), FileMode.Create)) { 
-                            file.CopyTo(filestream);
+                        using(var stream = file.OpenReadStream()) {
+                            blobClient.Upload(stream, overwrite: true);
                         }
 
                         ProductImage productImage = new() {
-                            ImageUrl=@"\"+productPath+@"\"+fileName,
-                            ProductId=productVM.Product.Id,
+                            ImageUrl = blobClient.Uri.ToString(),
+                            ProductId = productVM.Product.Id,
                         };
 
                         if(productVM.Product.ProductImages == null) {
-							productVM.Product.ProductImages=new List<ProductImage>();
-						}
+                            productVM.Product.ProductImages = new List<ProductImage>();
+                        }
                         productVM.Product.ProductImages.Add(productImage);
                     }
-                    unitOfWork.Product.Update(productVM.Product);
-					unitOfWork.Save();
-				}
 
-                TempData["success"] = "Product created /updated successfully!"; //temp data is used to preserve info until next load of page. If page is refreshed, data is loast
+                    unitOfWork.Product.Update(productVM.Product);
+                    unitOfWork.Save();
+                }
+
+                TempData["success"] = "Product created/updated successfully!";
                 return RedirectToAction("Index");
             } else {
-				productVM.CategoryList = unitOfWork
-                                        .Category
-                                        .GetAll()
-                                        .Select(u => new SelectListItem {
-                                            Text = u.Name,
-                                            Value = u.Id.ToString()
-                                        });
-				return View(productVM);
-			}
+                productVM.CategoryList = unitOfWork
+                    .Category
+                    .GetAll()
+                    .Select(u => new SelectListItem {
+                        Text = u.Name,
+                        Value = u.Id.ToString()
+                    });
+                return View(productVM);
+            }
         }
 
         [HttpPost, ActionName("Delete")]
-        public IActionResult DeletePOST(int? id)
-        {//delete get and post cannot be of same name since parameter is same. It will cause confusion
-         //So, we updated delete post method name and explicitely tell this end point action name is delete
-
+        public IActionResult DeletePOST(int? id) {
             Product? obj = unitOfWork.Product.Get(u => u.Id == id);
-            if (obj == null)
-            {
+            if(obj == null) {
                 return NotFound();
             }
             unitOfWork.Product.Remove(obj);
@@ -142,53 +129,54 @@ namespace ProjectCore.Areas.Admin.Controllers
         public IActionResult DeleteImage(int imageId) {
             var imageToBeDeleted = unitOfWork.ProductImage.Get(u => u.Id == imageId);
             int productId = imageToBeDeleted.ProductId;
+
             if(imageToBeDeleted != null) {
                 if(!string.IsNullOrEmpty(imageToBeDeleted.ImageUrl)) {
-                    var oldImagePath = Path.Combine(webHostEnvironment.WebRootPath,
-                        imageToBeDeleted.ImageUrl.TrimStart('\\'));
-                    if(System.IO.File.Exists(oldImagePath)) {
-                        System.IO.File.Delete(oldImagePath);
+                    // Only delete from blob if it's a blob URL
+                    if(imageToBeDeleted.ImageUrl.StartsWith("https://")) {
+                        var containerClient = GetBlobContainerClient();
+                        Uri uri = new Uri(imageToBeDeleted.ImageUrl);
+                        string blobName = string.Join("", uri.Segments.Skip(2));
+                        BlobClient blobClient = containerClient.GetBlobClient(blobName);
+                        blobClient.DeleteIfExists();
                     }
+                    // Old local path images are just removed from DB, file is already gone after redeployment
                 }
+
                 unitOfWork.ProductImage.Remove(imageToBeDeleted);
                 unitOfWork.Save();
-				TempData["success"] = "Deleted successfully!";
-			}
-            return RedirectToAction(nameof(Upsert),new {id=productId});
+                TempData["success"] = "Deleted successfully!";
+            }
+
+            return RedirectToAction(nameof(Upsert), new { id = productId });
         }
 
-		#region API CALLS
-		[HttpGet]
+        #region API CALLS
+        [HttpGet]
         public IActionResult GetAll() {
             List<Product> objProductList = unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
             return Json(new { data = objProductList });
         }
 
         [HttpDelete]
-		public IActionResult Delete(int? id) {
-
-            var productToBeDeleted = unitOfWork.Product.Get(u=>u.Id == id);
-            if (productToBeDeleted == null) {
-                return Json(new {success = false,message = "Error while deleting"});
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int? id) {
+            var productToBeDeleted = unitOfWork.Product.Get(u => u.Id == id);
+            if(productToBeDeleted == null) {
+                return Json(new { success = false, message = "Error while deleting" });
             }
 
-			string productPath = Path.Combine("images", "product", "product-" + id);
-			string finalPath=Path.Combine(webHostEnvironment.WebRootPath, productPath);
-
-            if(Directory.Exists(finalPath)) {
-                string[] filePaths = Directory.GetFiles(finalPath);
-                foreach (var filePath in filePaths)
-                {
-                    System.IO.File.Delete(filePath);
-                }
-                Directory.Delete(finalPath);
+            var containerClient = GetBlobContainerClient();
+            string prefix = $"product/product-{id}/";
+            await foreach(var blobItem in containerClient.GetBlobsAsync(traits: BlobTraits.None, states: BlobStates.All, prefix: prefix, cancellationToken: CancellationToken.None)) {
+                containerClient.GetBlobClient(blobItem.Name).DeleteIfExists();
             }
 
             unitOfWork.Product.Remove(productToBeDeleted);
             unitOfWork.Save();
 
-			return Json(new { success = true, message = "Delete Successful" });
-		}
-		#endregion
-	}
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+        #endregion
+    }
 }
